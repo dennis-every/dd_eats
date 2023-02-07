886365ae19c11a86224173ef1b9d927f0240054a
Dennis Every <dennis@MacBook-Air.local>
2023-02-07 18:17:39 -0400

    Fix loadMeals function name

--- src/modules/loadMeals.js
+++ src/modules/loadMeals.js
@@ -1,11 +1,11 @@
 import getMeals from './getMeals.js';
 import showMeal from './showMeal.js';
 
-const loadItems = async () => {
+const loadMeals = async () => {
   const meals = await getMeals();
   meals.forEach((meal) => {
     showMeal(meal);
   });
 };
 
-export default loadItems;
+export default loadMeals;
