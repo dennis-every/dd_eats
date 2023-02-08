85e1773f5c77eeb37b6bed78d48084358d4db947
Dennis Every <dennis@MacBook-Air.local>
2023-02-07 21:52:05 -0400

    Add getLikes, loadLikes, and showLike functionalities

--- src/modules/loadMeals.js
+++ src/modules/loadMeals.js
@@ -1,11 +1,13 @@
 import getMeals from './getMeals.js';
 import showMeal from './showMeal.js';
+import loadLikes from './loadLikes.js';
 
 const loadMeals = async () => {
   const meals = await getMeals();
   meals.forEach((meal) => {
     showMeal(meal);
   });
+  loadLikes();
 };
 
 export default loadMeals;
--- src/modules/showMeal.js
+++ src/modules/showMeal.js
@@ -6,19 +6,31 @@ const showMeal = (meal) => {
   const mealElement = document.createElement('li');
   mealElement.classList.add('col-md-6');
   mealElement.classList.add('col-lg-4');
-  mealElement.classList.add('col-xl-3');
+  mealElement.classList.add('col-xxl-3');
   mealElement.innerHTML = `
-  <article class='card'>
+  <article class='card' id=${meal.idMeal}>
     <img src=${meal.strMealThumb} class="card-img-top" alt=${meal.strMeal}>
     <div class="card-body">
-      <header class='d-flex justify-content-between'>
-        <h4>${meal.strMeal}</h4>
-        <a href="#"><i class="fa-regular fa-heart fa-2x"></i></a> 
+      <header class='row'>
+        <div class="col">
+          <span class='fs-3'>${meal.strMeal}</span><br> 
+          <span class="fs-6">${meal.strArea}</span>
+        </div>
+        <div class="col d-flex justify-content-end">
+          <div class="d-flex justify-content-center flex-column align-items-center"> 
+            <a href="#"><i class="fa-regular fa-heart fa-2x"></i></a>
+            <small id="likes_${meal.idMeal}">0 likes</small>
+          </div>
+        </div>
       </header>
-      <span>${meal.strArea}</span>
-      <div class="text-end">
-        <button id="commentsBtn" class='btn btn-outline-success'>Comments</button>
-      </div>  
+      <div class="row mt-3">
+        <div class="col">
+          <small>${meal.strCategory}</small>
+        </div>
+        <div class="col text-end">
+          <button id="comments_${meal.idMeal}" class='btn btn-outline-success commentsBtn'>Comments</button>
+        </div>
+      </div>
     </div>
   </article>  
   `;
