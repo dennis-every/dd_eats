326a6ee1b55e04b290d91311d9655d8e37dc017a
Dennis Every <dennis@MacBook-Air.local>
2023-02-07 23:42:51 -0400

    Add updateLikeCount function

--- src/modules/likes/addLike.js
+++ src/modules/likes/addLike.js
@@ -1,9 +1,11 @@
 import postLike from './api/postLike.js';
+import updateLikeCount from './updateLikeCount.js';
 
 const addLikeEventHandler = (e) => {
   e.preventDefault();
   const mealId = e.target.parentElement.id.split('_')[1];
   postLike(mealId);
+  updateLikeCount(mealId);
 };
 
 export const loadLikeEventListener = (meal) => {
