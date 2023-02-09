e6c40ee9baf85953a50bb2a95a43fb2c2d203d87
dfsalam <dfsalam@gmail.com>
2023-02-08 21:39:51 -0500

    Add-comments - Add postComments function. - Add event listener for the comment button(inside the popup). - Add some styling.

---
+++ src/modules/comments/api/postComments.js
@@ -0,0 +1,29 @@
+import loadComments from "../loadComments.js";
+
+const postComments = async (idMeal) => {
+  const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/MVXIkzjpFxBu0g7Zagmf/comments';
+    const user = document.getElementById('input__name');
+    const comment = document.getElementById('input__comment');
+    const dataToPost = {
+      'item_id': idMeal,
+      'username': user.value,
+      'comment': comment.value,
+    };
+    const response = await fetch(url, {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dataToPost),
+    });
+    loadComments(idMeal);
+    user.value = '';
+    comment.value = '';
+  
+    if (!response.ok) {
+      const message = `An error has occured: ${response.status}`;
+      throw new Error(message);
+    }
+  };
+  
+  export default postComments;
