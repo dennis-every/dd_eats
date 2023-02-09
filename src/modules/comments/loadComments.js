87997e5defb37aaf3cd436529a1aa190f3fe6cff
Dennis Every <dennis@ansyra.com>
2023-02-09 18:20:19 -0400

    Move date element to right, and added Readme.md

--- src/modules/comments/loadComments.js
+++ src/modules/comments/loadComments.js
@@ -1,26 +1,26 @@
 import getComments from './api/getComments.js';
 import commentsCounter from './commentsCounter.js';
 
-const loadComments = async (idMeals) => {  
-  const commentCntr = document.getElementById('comment__comment');  
+const loadComments = async (idMeals) => {
+  const commentCntr = document.getElementById('comment__comment');
   const comments = await getComments(idMeals);
   const container = document.getElementById('error__message');
-  container.textContent ='';      
-  let prevComments = ''; 
+  container.textContent = '';
+  let prevComments = '';
   if (comments) {
     comments.forEach((e) => {
       prevComments += `
         <li id='comment__card'>
           <h5><b>@${e.username}</b></h5>
           <h5>${e.comment}</h5>
-          <h6 id='date__text'>${e.creation_date}</h6>
+          <h6 id='date__text' class="text-end">${e.creation_date}</h6>
         </li>
       `;
     });
     commentCntr.innerHTML = prevComments;
   } else {
-    commentCntr.innerHTML = prevComments;    
-    container.textContent = 'There are no comments yet for this meal.';    
+    commentCntr.innerHTML = prevComments;
+    container.textContent = 'There are no comments yet for this meal.';
   }
   commentsCounter();
 };
