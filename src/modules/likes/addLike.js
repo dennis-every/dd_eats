294b47ed754432a095007cc35747689471cfce32
Dennis Every <dennis@ansyra.com>
2023-02-09 17:21:42 -0400

    Prevent additional likes after first like

--- src/modules/likes/addLike.js
+++ src/modules/likes/addLike.js
@@ -4,8 +4,12 @@ import updateLikeCount from './updateLikeCount.js';
 const addLikeEventHandler = (e) => {
   e.preventDefault();
   const mealId = e.target.parentElement.id.split('_')[1];
-  postLike(mealId);
-  updateLikeCount(mealId);
+  const liked = e.target.parentElement.classList.contains('liked');
+  if (!liked) {
+    e.target.parentElement.classList.add('liked');
+    postLike(mealId);
+    updateLikeCount(mealId);
+  }
 };
 
 export const loadLikeEventListener = (meal) => {
